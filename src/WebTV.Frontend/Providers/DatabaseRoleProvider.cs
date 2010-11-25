cac1216fa87eeb0cd9a9eb72bd2ac2748c7acd2d
Eelco van Veldhuizen <Eelcovanveldhuizen@gmail.com>
2010-11-25 13:04:59 +0100

    Added the providers folder with the database providers

---
+++ src/WebTV.Frontend/Providers/DatabaseRoleProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using WebTV.Frontend.Providers;
+using WebTV.Model;
+
+namespace WebTV.Frontend.Providers {
+    public class DatabaseRoleProvider : RoleProvider {
+        public override string[] GetRolesForUser(string username) {
+            var ctx = new WebTVContext();
+            var user = ctx.Customers.SingleOrDefault(s => s.Name == username);
+            if(user.IsAdmin)
+                return new string[] { "Administrator", "User" };
+            else
+                return new string[] { "User" };
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames) {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName {
+            get {
+                return "WebTV";
+            }
+            set { }
+        }
+
+        public override void CreateRole(string roleName) {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole) {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch) {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles() {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName) {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName) {
+            return !(roleName == "Administrator" && username != "Administrator");
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames) {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName) {
+            return new string[] { "Administrator", "User" }.Contains(roleName);
+        }
+    }
+}
