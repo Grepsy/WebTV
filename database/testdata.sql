f48910fec1784c51d51a1a7a35950a8b7e9bcccd
Eelco van Veldhuizen <Eelcovanveldhuizen@gmail.com>
2010-11-25 13:03:09 +0100

    RoleProvider now uses the IsAdmin boolean from the database

--- database/testdata.sql
+++ database/testdata.sql
@@ -1,6 +1,10 @@
 USE WebTV
 
-INSERT INTO Customer (Name, Password) VALUES ('Hans Pots', 'bananas')
+INSERT INTO Customer (Name, Password) VALUES ('Hans Pots', 'bananas',1)
+INSERT INTO Customer (Name, Password) VALUES ('Eelco', 'eelcotest',0)
+INSERT INTO Customer (Name, Password) VALUES ('Robert', 'robertest',1)
+INSERT INTO Customer (Name, Password) VALUES ('Bas', 'bastest',0)
+INSERT INTO Customer (Name, Password) VALUES ('Ambert', 'ambertest',1)
 
 INSERT INTO Animation (Name, Message, CustomerId) VALUES ('Autohandelaar', 'Nu een halve auto voor de prijs van twee!', 1)
 INSERT INTO Animation (Name, Message, CustomerId) VALUES ('Koekenbakker', 'Koeken nu voor twee euro!', 1)
