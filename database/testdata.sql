80f4968e39aa525ab1ad6faffa9f2d0d2f5d59c3
Eelco van Veldhuizen <Eelcovanveldhuizen@gmail.com>
2011-01-02 20:44:40 +0100

    Changed some test data

--- database/testdata.sql
+++ database/testdata.sql
@@ -6,8 +6,11 @@ INSERT INTO Customer (Name, Password, IsAdmin,Enabled) VALUES ('Robert', 'robert
 INSERT INTO Customer (Name, Password, IsAdmin,Enabled) VALUES ('Bas', 'bastest', 0, 1)
 INSERT INTO Customer (Name, Password, IsAdmin,Enabled) VALUES ('Ambert', 'ambertest', 1, 1)
 
-INSERT INTO Animation (Name, CustomerId) VALUES ('Autohandelaar', 1)
-INSERT INTO Animation (Name, CustomerId, MediaGroupedBy) VALUES ('Koekenbakker', 1, 4)
+INSERT INTO Animation (Name, CustomerId) VALUES ('multimate', 1)
+INSERT INTO Animation (Name, CustomerId, MediaGroupedBy) VALUES ('restaurant', 2, 4)
+
+INSERT INTO Animation (Name, CustomerId) VALUES ('makelaar', 3)
+INSERT INTO Animation (Name, CustomerId) VALUES ('autohandelaar', 4)
 
 INSERT INTO PropertyDescriptor (Name) VALUES ('Naam')
 INSERT INTO PropertyDescriptor (Name) VALUES ('Omschrijving')
